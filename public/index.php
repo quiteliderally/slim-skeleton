c24069c3329f1847f908e3af13d8805030d17353
Tim Ariyeh <tim.ariyeh@gmail.com>
2014-11-19 21:40:35 -0600

    Initial Import

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+define('BASE_DIR', realpath(__DIR__ . '/..'));
+
+require BASE_DIR . '/vendor/autoload.php';
+
+$app = new \Slim\Slim(array(
+    'view' => new \Slim\Views\Twig(),
+    'templates.path' => BASE_DIR . '/templates'
+));
+
+foreach(glob(BASE_SIR . '/config/*.php') as $c){
+  require $c;
+}
+
+require BASE_DIR . '/app/main.php';
+
+$app->run();
