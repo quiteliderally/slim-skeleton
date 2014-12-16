9319d010b0930eee710709a3bf7cee3f57349efd
Tim Ariyeh <tim.ariyeh@gmail.com>
2014-12-16 15:50:33 +0000

    Fix a typo

---
+++ config/00-log.php
@@ -0,0 +1,3 @@
+<?php
+
+ini_set('error_log', BASE_DIR  . '/log/php_errors.log');
--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../bootstrap/php';
+require __DIR__ . '/../bootstrap.php';
 
 $app = new \Slim\Slim(array(
     'view' => new \Slim\Views\Twig(),
